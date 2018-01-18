4be8a1aae82fc0529fd2d87505fac6fa1dfbaeb3
Vivek Pavaskar <vivek.pavaskar@gmail.com>
2018-01-18 22:40:13 +0530

    Registration form validation done

--- Pet House/pet.js
+++ Pet House/pet.js
@@ -11,6 +11,28 @@ function checklogin() {
 		lf.style.display="none";
 	}
 }
+function checkreg(){
+	var n=document.reg.fname.value;
+	var e=document.reg.email.value;
+	var u=document.reg.uid.value;
+	var p=document.reg.pass.value;
+	var r=document.reg.repass.value;
+	var at=e.indexOf('@');
+	var dot=e.lastIndexOf('.');
+	if (n==null || n=="") {
+		alert('Enter your name.');
+	} else if (u==null || u=="") {
+		alert('Enter a valid username.');
+	} else if (p.length<6) {
+		alert('Enter a password with at least 6 characters.');
+	} else if (p!=r) {
+		alert('password does not match');
+	} else if (at<1 ||dot<at+2 || dot+2>e.length) {
+		alert('Enter a valid email address');
+	} else {
+		return true;
+	}
+}
 
 var si=0;
 				slideshows();
